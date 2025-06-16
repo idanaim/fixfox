d87264485c36a652da9b6656ccbbffe1267dc6cf
Idan Naim <inaim@proofpoint.com>
2025-06-16 11:02:15 +0300

    Fixed workflow

--- apps/rest-man-server/src/app/app.controller.ts
+++ apps/rest-man-server/src/app/app.controller.ts
@@ -5,6 +5,9 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  /**
+   * Health check endpoint to verify the status of the REST API server.
+   */
   @Get('health')
   getHealth() {
     return {
